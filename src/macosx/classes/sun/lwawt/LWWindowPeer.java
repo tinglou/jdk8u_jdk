d1ceab758d1483e945ae3a0a509636a143e11fd6
Anton Tarasov <anton.tarasov@jetbrains.com>
2016-07-12 17:49:27 +0300

    JDK-8073320 Windows HiDPI Graphics support

--- src/macosx/classes/sun/lwawt/LWWindowPeer.java
+++ src/macosx/classes/sun/lwawt/LWWindowPeer.java
@@ -1110,7 +1110,9 @@ public class LWWindowPeer
             && !(dst instanceof NullSurfaceData)
             && !(src instanceof NullSurfaceData)
             && src.getSurfaceType().equals(dst.getSurfaceType())
-            && src.getDefaultScale() == dst.getDefaultScale()) {
+            && src.getDefaultScaleX() == dst.getDefaultScaleX()
+            && src.getDefaultScaleY() == dst.getDefaultScaleY())
+        {
             final Rectangle size = src.getBounds();
             final Blit blit = Blit.locate(src.getSurfaceType(),
                                           CompositeType.Src,
